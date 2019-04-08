51b5aa2e72325dc8ca6e5570ee3d00bbfffffd61
Sergej Grilborzer <sergej1995@gmail.com>
2019-04-08 16:50:57 +0200

    Init Spring Backend = BE

---
+++ Server-Backend-Spring/src/main/java/de/grilborzer/serverbackendspring/ServerBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package de.grilborzer.serverbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBackendSpringApplication.class, args);
+	}
+
+}
