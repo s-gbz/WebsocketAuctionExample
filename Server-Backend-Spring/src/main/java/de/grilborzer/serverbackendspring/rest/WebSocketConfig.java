30c3891d2f7448b0ec854b5c5eace240adc0b142
Sergej Grilborzer <sergej1995@gmail.com>
2019-04-15 14:15:59 +0200

    BE: Configure WebSocketConfig

--- Server-Backend-Spring/src/main/java/de/grilborzer/serverbackendspring/rest/WebSocketConfig.java
+++ Server-Backend-Spring/src/main/java/de/grilborzer/serverbackendspring/rest/WebSocketConfig.java
@@ -2,23 +2,25 @@ package de.grilborzer.serverbackendspring.rest;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/auction");
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
+    // Endpoint for client registry
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/socket-registry").withSockJS();
     }
 
+    // Endpoint for client topic subscription
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/update-items");
+    }
 }
