7a981f19c410daba6e5ec30c1aaeb87489432672
Sergej Grilborzer <sergej1995@gmail.com>
2019-07-16 13:26:03 +0200

    Remove use of environment files

--- Client-Frontend-Angular/src/app/http.service.ts
+++ Client-Frontend-Angular/src/app/http.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../src/environments/environment';
 import { AuctionItem } from './auction-item';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -11,19 +10,22 @@ export class HttpService {
 
   private getAllAuctionItemsUrl = "/all-items";
   private updateAuctionItemsUrl = "/item-updates";
+  private serverUrl: "http://localhost:8080";
+  private webSocketUrl: "ws://localhost:8080/socket-registry";
+
   private httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 
   constructor(private http: HttpClient) { }
 
   updateItem(item: AuctionItem): Observable<boolean> {
-    return this.http.post<boolean>(environment.serverUrl + this.updateAuctionItemsUrl, JSON.stringify(item), this.httpOptions);
+    return this.http.post<boolean>(this.serverUrl + this.updateAuctionItemsUrl, JSON.stringify(item), this.httpOptions);
   }
 
   getWebSocket(): WebSocket {
-    return new WebSocket(environment.webSocketUrl);
+    return new WebSocket(this.webSocketUrl);
   }
 
   initializeAuctionItems(): Observable<AuctionItem []> {
-    return this.http.get<AuctionItem []>(environment.serverUrl + this.getAllAuctionItemsUrl);
+    return this.http.get<AuctionItem []>(this.serverUrl + this.getAllAuctionItemsUrl);
   }
 }
--- Client-Frontend-Angular/src/environments/environment.prod.ts
+++ Client-Frontend-Angular/src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  serverUrl: "",
-  webSocketUrl: "ws://10.212.13.98:8080/socket-registry"
+  production: true
 };
